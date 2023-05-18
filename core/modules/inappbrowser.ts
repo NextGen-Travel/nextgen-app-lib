ae5b9087802c73b629314c8b82d139cac9979c61
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-05-18 12:41:19 +0800

    feat: 例行更新@1684384879

--- core/modules/inappbrowser.ts
+++ core/modules/inappbrowser.ts
@@ -1,13 +1,19 @@
 import { Capacitor } from '@capacitor/core'
 import { Browser } from '@capacitor/browser'
+import { AppLauncher } from '@capacitor/app-launcher'
 export class BrowserManager {
     static open(url: string, nameType: '_blank' | '_system' = '_blank') {
         let IsApp = Capacitor.isNativePlatform()
         if (IsApp) {
-            Browser.open({
-                url,
-                windowName: nameType
-            })
+            if (nameType === '_system') {
+                AppLauncher.openUrl({
+                    url
+                })
+            } else {
+                Browser.open({
+                    url
+                })
+            }
             return {}
         } else {
             window.open(url, nameType)
