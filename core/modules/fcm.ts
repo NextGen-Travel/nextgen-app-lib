baa136202315f424e968c27586514e6ca3b22b74
zhihao <zhihaozheng@nextgen.com.hk>
2023-10-25 11:28:26 +0800

    feat: 例行更新@1698204505

--- core/modules/fcm.ts
+++ core/modules/fcm.ts
@@ -1,21 +1,42 @@
 import '../index'
-import { FirebaseMessaging } from '@capacitor-firebase/messaging'
+import { Event } from 'power-helper'
+import { FirebaseMessaging, Notification } from '@capacitor-firebase/messaging'
 
 // TOOD: 有實際需求的時候再處理後續
+
 /**
  * @see https://capacitorjs.com/docs/apis/push-notifications
  * @see https://github.com/capacitor-community/fcm
  */
+
+const event = new Event<{
+    // 在應用程師外收到通知，點擊通知後會觸發
+    'notificationActionPerformed': {
+        notification: Notification
+    }
+    // 在應用程師內收到通知
+    'notificationReceived': {
+        notification: Notification
+    }
+}>()
+
 export class FcmManager {
     static async install() {
         const result = await FirebaseMessaging.requestPermissions()
-        await FirebaseMessaging.addListener('notificationActionPerformed', (notification) => {
-            console.log('notificationActionPerformed', notification)
+        await FirebaseMessaging.addListener('notificationActionPerformed', ({ notification }) => {
+            event.emit('notificationActionPerformed', {
+                notification: notification
+            })
         })
-        await FirebaseMessaging.addListener('notificationReceived', (notification) => {
-            console.log('notificationReceived', notification)
+        await FirebaseMessaging.addListener('notificationReceived', ({ notification }) => {
+            event.emit('notificationReceived', {
+                notification: notification
+            })
         })
         return result.receive
+    }
+    static get on() {
+        return event.on.bind(event)
     }
     static async clear() {
         const hasToken = await FirebaseMessaging.getToken()
